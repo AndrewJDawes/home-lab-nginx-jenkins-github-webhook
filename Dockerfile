35992bce4f76a99f2c211d18c1f82c6d46585537
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-02-05 20:01:38 -0500

    Make the proxy location dynamic

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,15 @@ FROM nginx:1.27.1-alpine AS base
 ENV NGINX_UPSTREAM_PROTOCOL=http
 ENV NGINX_UPSTREAM_PORT=80
 ENV NGINX_UPSTREAM_HOST="\$upstream_host"
+ENV NGINX_PROXY_PASS_LOCATION="/"
 
 EXPOSE 80
 
-COPY /etc/nginx/nginx.conf.template /etc/nginx/nginx.conf.template
+COPY ./ /home-lab-nginx-jenkins-public-routes
 
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
+WORKDIR /home-lab-nginx-jenkins-public-routes
 
-CMD ["/entrypoint.sh", "nginx", "-g", "daemon off;"]
+CMD ["/bin/sh", "entrypoint.sh", "nginx", "-g", "daemon off;"]
 
 # CMD ["nginx", "-g", "daemon off;"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
-envsubst '$NGINX_UPSTREAM_PROTOCOL $NGINX_UPSTREAM_HOST $NGINX_UPSTREAM_PORT' </etc/nginx/nginx.conf.template >/etc/nginx/nginx.conf
+# Loop through all .template files in /etc/templates/
+for template in ./templates/*.template; do
+    # Extract filename without the .template extension
+    output_file="/etc/nginx/$(basename "$template" .template)"
+
+    # Perform envsubst and write to the output file
+    envsubst "$(printf '${%s} ' $(env | cut -d= -f1))" <"$template" >"$output_file"
+
+    echo "Processed: $template -> $output_file"
+done
 exec "$@"
